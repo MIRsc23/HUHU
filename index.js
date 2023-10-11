8629b63b65c58f4bec4595a3826657c62488fe00
MIRsc23 <147380360+MIRsc23@users.noreply.github.com>
2023-10-11 19:46:24 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ function texty(number) {
     switch (number) {
         case 1:
             document.getElementById('replace-me').innerText = 'Ich dich auch!';
+            document.getElementById("replace-me").style.color = 'red';
+            document.getElementById("replace-me").style.fontSize = '20px';
+            document.getElementById("replace-me").style.marginLeft = '30%';
             break;
         case 2:
-            answer = "b";
+            document.getElementById('replace-me').innerText = 'Ein Bofel!';
+            document.getElementById("replace-me").style.color = 'purple';
+            document.getElementById("replace-me").style.fontSize = '40px';
+            document.getElementById("replace-me").style.marginLeft = '20%';
+            document.getElementById("replace-me").style.marginTop = '-75%';
+            break;
+        case 3:
+            document.getElementById('replace-me').innerText = 'Finde ich auch ... :)';
+            document.getElementById("replace-me").style.color = 'blue';
+            document.getElementById("replace-me").style.fontSize = '20px';
+            document.getElementById("replace-me").style.marginLeft = '20%';
+            document.getElementById("replace-me").style.marginTop = '-75%';
             break;
         default:
             answer = "c";
